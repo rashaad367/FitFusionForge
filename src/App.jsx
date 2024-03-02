19cda43755128db6aab41bb47fb030e21b2cb636
rashaad367 <rashaadjones367@gmail.com>
2024-03-02 16:26:08 -0500

    creating login form

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LoginBox from './components/LoginBox';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Cool story b</h1>
+      <LoginBox></LoginBox>
     </>
   )
 }
---
+++ src/components/LoginBox.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './LoginBox.css';
+
+function LoginBox() {
+
+    return (<><div className="box">
+        <h3>Login</h3>
+        <form action="" method="get">
+            <div className="emailpassword">
+                <input type="email" name="" id="" placeholder="Email"/>
+                <input type="password" name="" id="" placeholder="Password"/>
+            </div>
+        </form>
+        <h6>Don't have an account yet?</h6>
+        <a><h3>Sign up</h3></a>
+    </div></>)
+}
+
+export default LoginBox
