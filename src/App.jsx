b301804e7fa5bb15f98a754318e5e2663a713c15
rashaad367 <rashaadjones367@gmail.com>
2024-03-03 16:31:04 -0500

    adding scss, and pattern randomization

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss';
 import LoginBox from './components/LoginBox';
+import BoxPattern from './components/BoxPattern';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <LoginBox></LoginBox>
+      <BoxPattern></BoxPattern>
+ 
+       <LoginBox></LoginBox>
+     
     </>
   )
 }
---
+++ src/components/BoxPattern.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './BoxPattern.scss';
+
+function BoxPattern() {
+
+    return (<>
+        <div className="bgcircle"></div>
+        <div className="bgcircle"></div>
+        <div className="bgcircle"></div>
+        <div className="mdcircle"></div>
+        <div className="mdcircle"></div>
+        <div className="smcircle"></div>
+        <div className="smcircle"></div>
+        <div className="smcircle"></div>
+        <div className="bgrectangle"></div>
+        <div className="mdrectangle"></div>
+        <div className="mdrectangle"></div>
+    </>)
+}
+
+export default BoxPattern
