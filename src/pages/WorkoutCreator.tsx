ad2ac9d016ed4e98c75e40045b4ee9cdfc91c017
rashaad367 <rashaadjones367@gmail.com>
2024-03-12 23:58:20 -0400

    creating workout creator

--- src/pages/WorkoutCreator.tsx
+++ src/pages/WorkoutCreator.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import WC_Box from "../components/WC_Box/WC_Box";
 
 const WorkoutCreator: React.FC = () => {
     return (
-        <><h1>workout creator</h1></>
+        <><h1>workout creator</h1>
+          <WC_Box></WC_Box>
+        </>
     )
 };
 
